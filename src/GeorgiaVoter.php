e9e08beb0316a37549b698a76f81afb56b60a523
James Jones <jamjon3@gmail.com>
2015-10-27 21:29:00 -0400

    Some initial stuff

---
+++ src/GeorgiaVoter.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace CF\gavoters;
+
+/**
+ * Description of GeorgiaVoter
+ *
+ * @author james
+ */
+class GeorgiaVoter {
+    public $CountyCode;
+    public $RegistrationNbr;
+    public $VoterStatus;
+    public $VoterLastName;
+    public $VoterFirstName;
+    public $VoterMiddleMaiden;
+    public $VoterNameSuffix;
+    public $VoterNameTitle;
+    public $ResHouseNbr;
+    public $ResStreetName;
+    public $ResStreetSuffix;
+    public $ResAptUnitNbr;
+    public $ResCityName;
+    public $ResZip;
+    public $ResZipPlus4;
+    public $MidrFlag;
+    public $PollWorker;
+    public $TransactionCode;
+    public $TransactionYearMonth;
+    public $Filler1;
+    public $DateOfBirth;
+    public $RegistrationDate;
+    public $Race;
+    public $Gender;
+    public $Absentee;
+    public $LandDistrict;
+    public $LandLot;
+    public $OriginalRegDate;
+    public $StatusReason;
+    public $Filler2;
+    public $CountyPrecinctId;
+    public $CityPrecinctId;
+    public $CongressionalDistrict;
+    public $SenateDistrict;
+    public $HouseDistrict;
+    public $JudicialDistrict;
+    public $CommDistrict;
+    public $SchoolDistrict;
+    public $CountyDistAName;
+    public $CountyDistAValue;
+    public $CountyDistBName;
+    public $CountyDistBValue;
+    public $MunicipalName;
+    public $MunicipalCode;
+    public $WardCityCouncilName;
+    public $WardCityCouncil;
+    public $CitySchoolDistName;
+    public $CitySchoolDist;
+    public $CityDistAName;
+    public $CityDistAValue;
+    public $CityDistBName;
+    public $CityDistBValue;
+    public $CityDistCName;
+    public $CityDistCValue;
+    public $CityDistDName;
+    public $CityDistDValue;
+    public $DateLastVotedDate;
+    public $TypeOfElection;
+    public $PartyLastVoted;
+    public $LastContactDate;
+    public $MailHouseNbr;
+    public $MailStreetName;
+    public $MailStreetSuffix;
+    public $MailAptUnitNbr;
+    public $MailCity;
+    public $MailState;
+    public $MailZip;
+    public $MailZipPlus4;
+    public $Filler3;
+    public $MailAddress2;
+    public $MailAddress3;
+    public $MailCounty;
+    public $DateAdded;
+    public $DateChanged;
+    public $DistrictCombo;
+    public $ResBuilding;
+    public $MailRRPOBox;
+    public $CombinedStreetAddr;
+    public $ExportDate;
+}
